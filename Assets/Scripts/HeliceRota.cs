282524abf9f407d16e9fe327884cd15967d2d8f4
Joaquin Schopflocher <joacoschop@gmail.com>
2026-05-08 09:04:52 -0300

    limites del player y juego helice

---
+++ Assets/Scripts/HeliceRota.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliceRota : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float velocidadRotacion = 100f;
+
+    void Update()
+    {
+        transform.Rotate(velocidadRotacion * Time.deltaTime, 0, 0);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,17 +9,17 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Translate(-2, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        if((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && transform.position.x > -2)
         {
             transform.Translate(-2, 0, 0);
         }
-        if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        if((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && transform.position.x < 2)
         {
             transform.Translate(2, 0, 0);
         }
