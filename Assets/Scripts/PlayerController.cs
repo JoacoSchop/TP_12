f21a06e6e169b5b569ab385997873a1dcce9aff4
Joaquin Schopflocher <joacoschop@gmail.com>
2026-05-15 08:27:54 -0300

    terminado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.Translate(-2, 0, 0);
+
     }
 
     // Update is called once per frame
